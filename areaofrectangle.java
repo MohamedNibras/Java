8af1e2359150542833e7f4e2eb4c1f6da3d1a834
MohamedNibras <113166568+MohamedNibras@users.noreply.github.com>
2023-09-03 19:41:23 +0530

    Add files via upload

---
+++ areaofrectangle.java
@@ -0,0 +1,10 @@
+class AreaRectangle
+{
+public static void main(String args[])
+{
+double length=2.55,breadth=5.53,area;
+System.out.println("program to calculate the Area of Rectangle.");
+area = length*breadth;
+System.out.println("length= " + length + "breadth= " + breadth + " \n Area is : " +area);
+}
+}
---
+++ unary.java
@@ -0,0 +1,10 @@
+class GFG
+{
+public static void main(String args[])
+{
+int n1=20;
+System.out.println("number ="+n1);
+n1=-n1;
+System.out.println("result="+n1);
+}
+}
