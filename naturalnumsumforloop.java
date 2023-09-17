ed41576efd0ff547ff4298b0c9dbb81bd9719ccf
MohamedNibras <113166568+MohamedNibras@users.noreply.github.com>
2023-09-17 20:44:38 +0530

    Add files via upload

---
+++ naturalnumsumforloop.java
@@ -0,0 +1,14 @@
+class NaturalNumSumForLoop
+{
+public static void main(String args[])
+{
+int sum=0;
+System.out.println("Sum of Natural Numbers 1 - 10");
+
+for (int i=1;i<=10;i++)
+{
+sum=sum+i;
+}
+System.out.println("Sum is: "+sum);
+}
+}
